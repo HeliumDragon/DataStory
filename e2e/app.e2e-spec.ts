56b0c7997d6a981dd44d48cf99ac7ff213600c44
Jessie Shi <jess0407@users.noreply.github.com>
2018-03-09 17:52:31 +0000

    update to angular 5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { DataStoryPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('data-story App', function() {
-  let page: DataStoryPage;
+describe('data-story App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new DataStoryPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { ChartModule } from 'angular2-highcharts'; 
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-
-  constructor(){
-    this.options = {
-      title : { text : 'simple chart' },
-      series: [{
-          data: [29.9, 71.5, 106.4, 129.2],
-      }]
-    };
-  }
-
-  title = 'This is a Data Story.';
-  options:Object;
-
+export class AppComponent {
+  title = 'app';
 }
