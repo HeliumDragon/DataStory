b28ef52065e0d9373f868d26d863743756b92326
Jessie Shi <jess0407@users.noreply.github.com>
2018-03-12 11:34:37 +0000

    add api setting to environment

--- src/app/core/services/movies.service.ts
+++ src/app/core/services/movies.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment'
 // import { Book } from '../../movies/models/movie';
 
 @Injectable()
 export class MovieService {
-  private API_PATH = 'https://0kwp7m4pz6.execute-api.eu-west-2.amazonaws.com/dev/movies';
+  private API_PATH = `${environment.api}movies`;
 
   constructor(private http: HttpClient) {}
 
